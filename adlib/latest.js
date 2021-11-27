4f31b5cff9ceb301c80960eeb696ee1f8fcc0c88
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2021-11-27 14:49:25 -0500

    Update latest.js

--- adlib/latest.js
+++ adlib/latest.js
@@ -5,8 +5,18 @@ var AdamRaichuJsLib = function() {
   this.authors = [
     "AdamRaichu"
     ]
-  this.version = "1.3.0"
-  this.lastUpdated = "11/24/21"
+  this.version = "1.4.1"
+  this.lastUpdated = "11/27/21"
+  this.availableFunctions = [
+  //about(); help(); local.array.store(); local.array.get(); edit(); id()
+    "about()",
+    "help()",
+    "local.array.store()",
+    "local.array.get()",
+    "edit()",
+    "id()",
+    "class()"
+  ]
   
   this.local = {
     array: {
@@ -17,7 +27,8 @@ var AdamRaichuJsLib = function() {
         return JSON.parse(localStorage.getItem(key))
       }
     }
-  }
+  } //end this.local
+  
 }
 
 AdamRaichuJsLib.prototype.onLoad = function() {
@@ -28,6 +39,10 @@ AdamRaichuJsLib.prototype.onLoad = function() {
     console.warn("%cA newer version of AdLib.js by AdamRaichu is available on GitHub @ AdamRaichu/code/adlib. Run AR.edit() to see current version.", "color: #0624b8")
   }
   
+}
+
+AdamRaichuJsLib.prototype.class = function (classNames) {
+  return document.getElementsByClassName(classNames)
 }
 
 AdamRaichuJsLib.prototype.id = function (elementId) {
@@ -44,7 +59,7 @@ AdamRaichuJsLib.prototype.about = function() {
   console.log("Authors: " + this.authors.join(", "))
   console.log("Version: " + this.version)
   console.log("Last Updated: " + this.lastUpdated)
-  console.log("Available Functions: about(); help(); local.array.store(); local.array.get(); edit(); id()")
+  console.log("Available Functions: " + this.availableFunctions.join("; "))
 }
 
 AdamRaichuJsLib.prototype.help = function(helpWithWhat) {
@@ -62,6 +77,8 @@ AdamRaichuJsLib.prototype.help = function(helpWithWhat) {
     console.log("This function is performed on the AR object when it is created. It logs version and checks for updates.")
   } else if (helpWithWhat === "id") {
     console.log("This function is a shortened version of document.getElementById(). It does the same thing, with no added functionality.")
+  } else if (helpWithWhat === "class") {
+    console.log("This function is a shortened version of document.getElementsByClassName(). It does the same thing, with no added functionality.")
   } else {
       //if no matches were found
     console.error('The syntax you used wasn\'t recognized by AR.help(). Run AR.help("help") for more information.')
