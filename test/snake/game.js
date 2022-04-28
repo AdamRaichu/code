134cf24b45d18d71e00366307cfa0f73e2018440
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-04-28 14:17:30 -0400

    Added username input.

--- test/snake/game.js
+++ test/snake/game.js
@@ -66,7 +66,7 @@ function playSnake() {
       localStorage.setItem("moves", JSON.stringify(moves))
       localStorage.setItem("apples", JSON.stringify(apples))
       $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
-        document.getElementById("hcb_form_name").value = data.ipAddress;
+        document.getElementById("hcb_form_name").value = AR.id("username").value;
       })
       document.getElementById("hcb_form_content").value = highscore
       document.getElementById("hcb_submit").click()
