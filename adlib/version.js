7ab724d0d53e13744cd50137171abcf874f26bb8
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-04-29 14:50:15 -0400

    Update version.js

--- adlib/version.js
+++ adlib/version.js
@@ -1,7 +1,7 @@
 var AdLibCurrentVersion = "2" //deprecated
 var AdamRaichuJsLibInfo = {
-    currentVersion : "2.0.1",
-    versionSplit: [2,0,1],
+    currentVersion : "2.1.0",
+    versionSplit: [2,1,0],
     urls: {
         code: "https://github.com/AdamRaichu/code/tree/main/adlib",
         newIssue: "https://github.com/AdamRaichu/code/issues/new",
