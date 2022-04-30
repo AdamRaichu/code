e76f0b8424313ef84b56559c9705ac39d87bb714
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-04-30 10:40:23 -0400

    added function which loads external script

--- api/chess/profile/getinfo.js
+++ api/chess/profile/getinfo.js
@@ -1,4 +1,9 @@
+function setActive(object) {
+  window.userInfo = object
+}
+
 var search = location.search.substring(1);
 var params = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-
 var username = params.username
+
+$.getScript("https://api.chess.com/pub/player/" + username + "?callback=setActive")
