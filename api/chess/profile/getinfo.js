14dcd3627e9fef267d464b5d72f898df78047323
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-04-30 14:44:38 -0400

    Delete api/chess/profile directory

--- api/chess/profile/getinfo.js
+++
@@ -1,9 +0,0 @@
-function setActive(object) {
-  window.userInfo = object
-}
-
-var search = location.search.substring(1);
-var params = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-var username = params.username
-
-$.getScript("https://api.chess.com/pub/player/" + username + "?callback=setActive")
