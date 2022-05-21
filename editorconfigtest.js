db9be9c3dfdfab0ee455907df47f51d23252c992
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-21 11:40:45 -0400

    delete unnecessary files

--- editorconfigtest.js
+++
@@ -1,4 +0,0 @@
-console.log("This file is intentionally formatted badly.")
-          function () {
-                                  console.log("Like this.")
-}
--- global.js
+++
@@ -1,6 +0,0 @@
-var user = localStorage.getItem("user")
-if (user !== null) {
-  console.log("%cWelcome " + user, "color: darkblue")
-} else {
-  console.log("You can set a user at adamraichu.github.io/code/user although this is not required.")
-}
