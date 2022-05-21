0aa4dddf83365c8f2ed352fa122c42bb371b1335
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-21 10:42:51 -0400

    updated build-version.js

--- build-version.js
+++ build-version.js
@@ -1,1 +1,5 @@
-console.log("build-version.js")
+var content = 'var AdLibCurrentVersion = "2";\nvar AdamRaichuJsLibInfo = {\n  currentVersion: "2.3.0",\n  versionSplit: [2, 3, 0],\n  urls: {\n    code: "https://github.com/AdamRaichu/code/tree/main/adlib",\n    newIssue: "https://github.com/AdamRaichu/code/issues/new",\n    readMe: "https://github.com/AdamRaichu/code/blob/main/adlib/README.md#supported-versions"\n  }\n}'
+
+console.log("==========")
+console.log(content)
+console.log("==========")
