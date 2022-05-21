90c4f1c4b7960049862d2a2d7e9d7cd4bcbb0fa6
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-21 11:30:28 -0400

    went to live test

--- build-version.js
+++ build-version.js
@@ -3,7 +3,7 @@ var version = process.env['npm_package_version']
 
 var content = 'var AdLibCurrentVersion = "2";\nvar AdamRaichuJsLibInfo = {\n  currentVersion: "' + version + '",\n  versionSplit: [' + version.split(".") + '],\n  urls: {\n    code: "https://github.com/AdamRaichu/code/tree/main/adlib",\n    newIssue: "https://github.com/AdamRaichu/code/issues/new",\n    readMe: "https://github.com/AdamRaichu/code/blob/main/adlib/README.md#supported-versions"\n  }\n}'
 
-fs.writeFile('adlib/version-test.txt', content, err => {
+fs.writeFile('adlib/version.js', content, err => {
     if (err) {
         console.error(err);
     }
