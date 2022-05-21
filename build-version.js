a7bacb14cb9ed1ac6b5a23de9eb2c4e0d40c9d48
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-21 10:50:12 -0400

    created version-test.txt

--- build-version.js
+++ build-version.js
@@ -1,5 +1,12 @@
-var content = 'var AdLibCurrentVersion = "2";\nvar AdamRaichuJsLibInfo = {\n  currentVersion: "2.3.0",\n  versionSplit: [2, 3, 0],\n  urls: {\n    code: "https://github.com/AdamRaichu/code/tree/main/adlib",\n    newIssue: "https://github.com/AdamRaichu/code/issues/new",\n    readMe: "https://github.com/AdamRaichu/code/blob/main/adlib/README.md#supported-versions"\n  }\n}'
+var content = 'var AdLibCurrentVersion = "2";\nvar AdamRaichuJsLibInfo = {\n  currentVersion: "' + process.env['npm_package_version'] + '",\n  versionSplit: [2, 3, 0],\n  urls: {\n    code: "https://github.com/AdamRaichu/code/tree/main/adlib",\n    newIssue: "https://github.com/AdamRaichu/code/issues/new",\n    readMe: "https://github.com/AdamRaichu/code/blob/main/adlib/README.md#supported-versions"\n  }\n}'
 
-console.log("==========")
-console.log(content)
-console.log("==========")
+fs.writeFile('adlib/version-test.txt', content, err => {
+    if (err) {
+        console.error(err);
+    }
+    console.log("text.txt file contents:")
+    console.log("==========")
+    console.log(content)
+    console.log("==========")
+    // file written successfully
+});
