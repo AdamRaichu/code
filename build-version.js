485a91c10fa366d97e7292b1e9227248c5afa53e
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-21 10:54:23 -0400

    added require(fs) statement

--- build-version.js
+++ build-version.js
@@ -1,3 +1,5 @@
+var fs = require("fs")
+
 var content = 'var AdLibCurrentVersion = "2";\nvar AdamRaichuJsLibInfo = {\n  currentVersion: "' + process.env['npm_package_version'] + '",\n  versionSplit: [2, 3, 0],\n  urls: {\n    code: "https://github.com/AdamRaichu/code/tree/main/adlib",\n    newIssue: "https://github.com/AdamRaichu/code/issues/new",\n    readMe: "https://github.com/AdamRaichu/code/blob/main/adlib/README.md#supported-versions"\n  }\n}'
 
 fs.writeFile('adlib/version-test.txt', content, err => {
