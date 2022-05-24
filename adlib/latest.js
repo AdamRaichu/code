332f64e1c2eff4cdd178383321655c66896d0d46
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-24 11:20:18 -0400

    add 2-4-0. This fixes Storage.setVar and getVar

--- adlib/latest.js
+++ adlib/latest.js
@@ -1,35 +1,37 @@
 // github.com/AdamRaichu/code
 // To be included with version.js on GitHub @ AdamRaichu/code/adlib/version.js
 
-var AdamRaichuJsLib = function() {
+var AdamRaichuJsLib = function () {
   this.authors = [
     "AdamRaichu"
   ]
-  this.version = "2.3.0",
-  this.versionSplit = [2,3,0]
-  this.lastUpdated = "5/12/22"
+  this.version = "2.4.0",
+    this.versionSplit = [2, 4, 0]
+  this.lastUpdated = "5/24/22"
   this.availableFunctions = [
     "about",
     "help",
     "id",
     "class",
-    "Storage.setVar",
-    "Storage.getVar",
+    "localStorage.setVar",
+    "localStorage.getVar",
+    "sessionStorage.setVar",
+    "sessionStorage.getVar",
     "randBetween",
     "CanvasRenderingContext2D.drawGrid",
     "newEl",
     "$",
     "$$"
   ]
-  
+
   this.onLoad()
-  
+
 }
 
-AdamRaichuJsLib.prototype.onLoad = function() {
-  
+AdamRaichuJsLib.prototype.onLoad = function () {
+
   console.info("%cAdamRaichu adlib.js is running. View at github.com/AdamRaichu/code", "color: #0624b8; background-color: #d8ae0d;")
-  
+
   //Check if a newer version is available.
   try {
     for (l = 0; l < AdamRaichuJsLibInfo.versionSplit.length; l++) {
@@ -45,19 +47,19 @@ AdamRaichuJsLib.prototype.onLoad = function() {
   }
 }
 
-AdamRaichuJsLib.prototype.$$ = function(selectors) {
+AdamRaichuJsLib.prototype.$$ = function (selectors) {
   return document.querySelectorAll(selectors)
 }
 
-AdamRaichuJsLib.prototype.$ = function(selectors) {
+AdamRaichuJsLib.prototype.$ = function (selectors) {
   return document.querySelector(selectors)
 }
 
-AdamRaichuJsLib.prototype.newEl = function(tagName) {
+AdamRaichuJsLib.prototype.newEl = function (tagName) {
   return document.createElement(tagName)
 }
 
-CanvasRenderingContext2D.prototype.drawGrid = function(increment) {
+CanvasRenderingContext2D.prototype.drawGrid = function (increment) {
   if (increment <= 0) {
     console.error("Increment must be greater than 0.")
     return
@@ -83,12 +85,20 @@ AdamRaichuJsLib.prototype.randBetween = function (low, high) {
   }
 }
 
-Storage.__proto__.setVar = function (key, variable) {
-  this.setItem(key, JSON.stringify(variable))
+localStorage.__proto__.setVar = function (key, variable) {
+  localStorage.setItem(key, JSON.stringify(variable))
 }
 
-Storage.__proto__.getVar = function (key) {
-  return JSON.parse(this.getItem(key))
+localStorage.__proto__.getVar = function (key) {
+  return JSON.parse(localStorage.getItem(key))
+}
+
+sessionStorage.__proto__.setVar = function (key, variable) {
+  sessionStorage.setItem(key, JSON.stringify(variable))
+}
+
+sessionStorage.__proto__.getVar = function (key) {
+  return JSON.parse(localStorage.getItem(key))
 }
 
 AdamRaichuJsLib.prototype.class = function (classNames) {
@@ -99,7 +109,7 @@ AdamRaichuJsLib.prototype.id = function (elementId) {
   return document.getElementById(elementId)
 }
 
-AdamRaichuJsLib.prototype.about = function() {
+AdamRaichuJsLib.prototype.about = function () {
   console.log("Listing information about this library...")
   console.log("Authors: " + this.authors.join(", "))
   console.log("Version: " + this.version)
@@ -107,7 +117,7 @@ AdamRaichuJsLib.prototype.about = function() {
   console.log("Available Functions: " + this.availableFunctions.join("; "))
 }
 
-AdamRaichuJsLib.prototype.help = function() {
+AdamRaichuJsLib.prototype.help = function () {
   window.open("https://github.com/AdamRaichu/code/wiki/AdLib.js")
 }
 
