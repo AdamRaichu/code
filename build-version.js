68b75cc187c192e48c52ab9fda3ac5853f295936
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-25 14:47:42 -0400

    added github-actions.log

--- build-version.js
+++ build-version.js
@@ -1,3 +1,40 @@
+function logAction(message, logLevel) {
+    fs.readFile("github-actions.log", (err, buff) => {
+        // if any error
+        if (err) {
+            console.error("[!!! ----- logAction failed ----- !!!]")
+            console.error(err);
+            return;
+        }
+
+        // otherwise log contents
+        var content = buff.toString();
+        content += "\n"
+        content += new Date()
+        content += " ["
+        if (logLevel === 3) {
+            content += "ERROR] "
+        } else if (logLevel === 2) {
+            content += "WARN] "
+        } else if (logLevel === 1) {
+            content += "INFO] "
+        } else {
+            content += "INFO] "
+            logAction("`logLevel` was eithere invalid or not provided. Source: build-version.js", 2)
+        }
+        content += message
+
+        fs.writeFile('github-actions.log', content, err => {
+            if (err) {
+                console.error("[!!! ----- logAction failed ----- !!!]")
+                console.error(err);
+            }
+            console.log("action logged")
+            // file written successfully
+        });
+    });
+}
+
 var fs = require("fs")
 var version = process.env['npm_package_version']
 
@@ -7,9 +44,11 @@ fs.writeFile('adlib/version.js', content, err => {
     if (err) {
         console.error(err);
     }
-    console.log("version.js file contents:")
-    console.log("==========")
-    console.log(content)
-    console.log("==========")
+    // console.log("version.js file contents:")
+    // console.log("==========")
+    // console.log(content)
+    // console.log("==========")
     // file written successfully
 });
+
+logAction("Built version.js with the following version: " + version, 1)
