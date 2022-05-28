fd69b995b4c510fbe71a14c7bd81f17f01a92a97
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-05-28 09:14:16 -0400

    renamed logAction.js to tools.js

--- build-version.js
+++ build-version.js
@@ -1,5 +1,5 @@
 var fs = require("fs")
-var tools = require('./logAction.js')
+var tools = require('./tools.js')
 var version = process.env['npm_package_version']
 
 var content = 'var AdLibCurrentVersion = "2";\nvar AdamRaichuJsLibInfo = {\n  currentVersion: "' + version + '",\n  versionSplit: [' + version.replace(/\./g, ", ") + '],\n  urls: {\n    code: "https://github.com/AdamRaichu/code/tree/main/adlib",\n    newIssue: "https://github.com/AdamRaichu/code/issues/new",\n    readMe: "https://github.com/AdamRaichu/code/blob/main/adlib/README.md#supported-versions"\n  }\n};'
